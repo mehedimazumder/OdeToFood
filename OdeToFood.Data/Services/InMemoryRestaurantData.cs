6b01d846321dc076ba199bc1ec08fab8c26288fe
mehedi <mehedirumc12cuet14@gmail.com>
2020-05-11 19:18:17 +0600

    first commit

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,24 @@
+﻿using OdeToFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurant
+    {
+        List<Restaurant> restaurents;
+        public InMemoryRestaurantData()
+        {
+            restaurents = new List<Restaurant>
+            {
+                new Restaurant {Id=1, Name="Bacha Baba", Cuisine= CuisineType.None},
+                new Restaurant {Id=1, Name="North Canteen", Cuisine= CuisineType.Indian},
+                new Restaurant {Id=1, Name="Kashem Rupali", Cuisine= CuisineType.French}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurents.OrderBy(r => r.Name);
+        }
+    }
+}
---
+++ OdeToFood.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        InMemoryRestaurantData Db;
+
+        public HomeController()
+        {
+            Db = new InMemoryRestaurantData();
+        }
+        public ActionResult Index()
+        {
+            var res = Db.GetAll();
+            return View(res);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
